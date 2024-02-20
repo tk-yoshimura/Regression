1ef843ab335eab3ea2083e0a9dce9e40f6f0d170
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-20 23:44:58 +0900

    add: asminfo

---
+++ Regression/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Regression")]
+[assembly: AssemblyDescription("Regression Analysis Utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Regression")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("CCD895F2-84F5-4027-8AC1-BFE2C77A609B")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+
+[assembly: InternalsVisibleTo("RegressionTest")]
